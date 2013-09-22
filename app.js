80ec7b5bf992eae8e241a28337f4a31f125984ab
Kyle Curren <ksc4130@gmail.com>
2013-09-22 12:27:52 -0400

    init

---
+++ app.js
@@ -0,0 +1,162 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 4130);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(require('less-middleware')({ src: __dirname + '/public' }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+app.post('/yup', function(req, res){
+
+});
+
+var server = http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+
+var gpio = require("gpio"),
+    floodLightsSwitch
+    , lightsSwitch
+    , pin = '41300048';
+
+var deviceIdCnt = 0;
+var devices = {};
+
+var device = function (pin, args) {
+    if(!pin || typeof pin !== 'number')
+        throw {
+            name: 'invalid device pin/id',
+            message: 'invalid device pin/id'
+        };
+
+    var self = new function() {};
+
+    args = args || {};
+
+    self.id = pin;
+    self.name = args.name || 'unknown';
+    self.state = args.state || 0;
+    self.direction = args.direction || 'out';
+
+    devices[pin.toString()] = gpio.export(pin, {
+        direction: args.direction || 'out',
+        interval: 200,
+        ready: function() {
+            devices[pin.toString()].on("change", function(val) {
+                self.state = val;
+            });
+        }
+    });
+
+    return self;
+};
+
+var barn = [
+    device(27, {
+        name: 'Lights'
+    }),
+    device(17, {
+        name: 'Flood Lights'
+    })
+];
+
+var floodLights = gpio.export(17, {
+    direction: 'out',
+    interval: 200,
+    ready: function() {
+    }
+});
+
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function (socket) {
+    var yup = false;
+    socket.emit('yup', false);
+    socket.on('yup', function (data) {
+        yup = (data === pin);
+        if(yup)
+            socket.emit('init', barn);
+        else
+            socket.emit('yup', false);
+    });
+    socket.on('change', function (data) {
+        if(!yup) {
+            socket.emit('yup', false);
+            return;
+        }
+        var device = devices[data.id];
+        if(device)
+            device.set(data.state, function() {
+                console.log(device.value);
+                io.sockets.emit('change', {id: data.id, state: device.value});
+            });
+        else
+            console.log("can't find device for id ", data.id);
+    });
+});
+
+floodLightsSwitch = gpio.export(22, {
+    ready: function() {
+        floodLightsSwitch.on("change", function(val) {
+            if(val === 1)
+                return;
+
+            var device = devices[17];
+
+            if(device)
+                device.set((1 - device.value), function() {
+                    console.log(device.value);
+                    io.sockets.emit('change', {id: 17, state: device.value});
+                });
+            else
+                console.log("can't find device for id ", 17);
+        });
+    }
+});
+
+lightsSwitch = gpio.export(23, {
+    ready: function() {
+        lightsSwitch.on("change", function(val) {
+            if(val === 1)
+                return;
+
+            var device = devices[27];
+
+            if(device)
+                device.set((1 - device.value), function() {
+                    console.log(device.value);
+                    io.sockets.emit('change', {id: 27, state: device.value});
+                });
+            else
+                console.log("can't find device for id ", 27);
+        });
+    }
+});
---
+++ public/javascripts/app.js
@@ -0,0 +1,71 @@
+var socket = io.connect(window.location.origin);
+
+var device = function (args) {
+
+    var self = new function() {};
+
+    args = args || {};
+
+    self.id = args.id;
+    self.name = args.name || 'unknown';
+    self.state = ko.observable(args.state || 0);
+
+    self.isOn = ko.computed(function () {
+        return self.state() === 1;
+    });
+
+    self.isOff = ko.computed(function () {
+        return self.state() === 0;
+    });
+
+    self.toggle = function () {
+        socket.emit('change', {
+            id: self.id,
+            state: (1 - self.state())
+        });
+    };
+
+    return self;
+};
+var $yup = $('#yup');
+var vm = new function () {
+    var self = this;
+    self.barn = ko.observableArray([]);
+    self.pin = ko.observable();
+
+    self.yup = function () {
+        $yup.modal('hide');
+        socket.emit('yup', self.pin());
+    };
+};
+socket.on('yup', function (data) {
+    if(!data) {
+        $yup.modal('show');
+    }
+});
+
+socket.on('init', function (data) {
+    var mapped = ko.utils.arrayMap(data, function (item) {
+        return device(item);
+    });
+    vm.pin(undefined);
+    vm.barn(mapped);
+
+    $('#content').show();
+});
+
+socket.on('change', function (data) {
+    var arr = vm.barn(),
+        device = ko.utils.arrayFirst(arr, function (item) {
+            return item.id === data.id;
+        });
+    if(device)
+        device.state(data.state);
+});
+
+$(function () {
+    $yup.modal({
+        show: false
+    });
+    ko.applyBindings(vm);
+});
